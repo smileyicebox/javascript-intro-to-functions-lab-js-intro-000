335b426ca001d0bee9c84528b1e164e191c0aa1d
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-17 22:23:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
     return 'I can\'t hear you!'
   }
-  if (string === "I love you, Grandma") {
+  if (string === 'I love you, Grandma') {
     return 'I love you, too'
   }
 }
